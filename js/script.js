359a6cf9fd22e6fc3b17ea8716bea4fa65a92786
Sunammi70 <105335044+Sunammi70@users.noreply.github.com>
2022-11-09 01:05:14 +0200

    Unzalingei

---
+++ js/script.js
@@ -0,0 +1,25 @@
+searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    searchForm.classList.remove('active')
+    if (window.scrollY > 120) {
+        document.querySelector('.header .header-2').classList.add('active');
+    }
+    else {
+        document.querySelector('.header .header-2').classList.remove('active');
+
+    }
+}
+window.onload = () => {
+
+    if (window.scrollY > 120) {
+        document.querySelector('.header .header-2').classList.add('active');
+    }
+    else {
+        document.querySelector('.header .header-2').classList.remove('active');
+
+    }
+}
